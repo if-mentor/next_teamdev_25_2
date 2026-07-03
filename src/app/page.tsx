6af8d0010d0a4f6d557fe73f191bdd2077160b1b
risenagata <okkiy0526@gmail.com>
2026-07-03 23:29:45 +0900

    page.tsxからCardを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Card from "@/components/Card";
 import styles from "./styles.module.css";
 
 export default function Home() {
   return (
     <div>
       <h1 className={styles.title}>Hello Teamdev!!</h1>
-
-      <Card
-        title="チーム開発"
-        author="山田太郎"
-        category="プログラミング"
-        thumbnailUrl="/sample1.jpg"
-        content="これは例です。これは例です。これは例です。これは例です。これは例です。これは例です。これは例です。これは例です。"
-        createdAt="2 min ago"
-      />
     </div>
   );
 }
