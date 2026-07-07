c88dfff32f2c9724971f16cebf12fe2198d045ba
risenagata <okkiy0526@gmail.com>
2026-07-07 22:23:51 +0900

    page.tsxよりCardコンポーネントの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import Card from "@/components/Card";
-
 export default function Home() {
-  return (
-    <>
-      <Card
-        title="記事タイトル"
-        author="山田 太郎"
-        category="テック"
-        thumbnailUrl="/sample1.jpg"
-        content="この記事では～について解説します"
-        createdAt="2026-05-31"
-      />
-    </>
-  );
+  return <></>;
 }
