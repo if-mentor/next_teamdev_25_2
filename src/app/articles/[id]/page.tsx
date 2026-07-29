8b9f5ab308542278910c76733fb5019d88cf8c77
ryo033141 <ryo033141@gmail.com>
2026-07-29 23:02:47 +0900

    レビュー内容を修正

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -8,40 +8,59 @@ export default function ArticleDetail() {
   return (
     <>
       <div className={styles.main}>
-        <div className={styles.flex}>
-          <p className={styles.title}>Blog Title</p>
-          <div className={styles.authoricon}>
-            <p className={styles.author}>Author</p>
-            <Image src="/default_user_icon.png" alt="アイコン" width={32} height={32} />
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <p className={styles.title}>Blog Title</p>
+
+            <div className={styles.authorIcon}>
+              <p className={styles.author}>Author</p>
+              <Image src="/default_user_icon.png" alt="アイコン" width={32} height={32} />
+            </div>
+          </div>
+
+          <Image className={styles.articlePicture} src="/sample1.jpg" alt="記事写真" width={640} height={320} />
+
+          <div className={styles.categoryWrapper}>
+            <p className={styles.category}>Category</p>
+          </div>
+
+          <div className={styles.text}>
+            <p>こんにちは〜についてのブログを書きます。</p>
+            <p>今日はいいお天気で〜をして楽しみました。</p>
+          </div>
+
+          <div className={styles.text}>
+            <p>とてもいい事がありまして、詳細は〜です。</p>
+            <p>最後まで読んで下さり、ありがとうございました。</p>
+          </div>
+
+          <div className={styles.buttonWrapper}>
+            <Button label="編集" variant="success" size="medium" />
+          </div>
+
+          <div className={styles.timeWrapper}>
+            <p className={styles.time}>a min ago</p>
           </div>
         </div>
-        <Image className={styles.articlepicture} src="/sample1.jpg" alt="記事写真" width={640} height={320} />
-        <p className={styles.category}>Category</p>
-        <div className={styles.divcontent}>
-          <p>こんにちは〜についてのブログを書きます。</p>
-          <p>今日はいいお天気で〜をして楽しみました。</p>
-        </div>
-        <div className={styles.divcontent}>
-          <p>とてもいい事がありまして、詳細は〜です。</p>
-          <p>最後まで読んで下さり、ありがとうございました。</p>
-        </div>
-        <div className={styles.button}>
-          <Button label="編集" variant="success" size="medium" />
-        </div>
-        <p className={styles.time}>a min ago</p>
       </div>
-      <p className={styles.comment}>◯◯件のコメント</p>
-      <div className={styles.commentform}>
+
+      <div className={styles.commentWrapper}>
+        <p className={styles.comment}>◯◯件のコメント</p>
+      </div>
+
+      <div className={styles.commentForm}>
         <CommentForm />
       </div>
-      <div className={styles.commetcard}>
+
+      <div className={styles.commentCard}>
         <CommentCard
           userName="テスト 太郎"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula nibh, interdum non enim sit amet, iaculis aliquet nunc."
           createdDate="2026-07-23T05:12:00+09:00"
         />
       </div>
-      <div className={styles.commetcard}>
+
+      <div className={styles.commentCard}>
         <CommentCard
           userName="テスト 太郎"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula nibh, interdum non enim sit amet, iaculis aliquet nunc."
